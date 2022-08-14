a365c3a247314fe9ecc4404cf5ee1541082f9fb6
Ben West <benwest0291@outlook.com>
2022-08-14 19:10:22 +0100

    made footer mobile responsice

--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
   <div class="main__footer">
     <div class="container">
     <div class="row">
-      <div class="col-12 col-6 col-md-4">
+      <div class="col-12 col-6 col-md-4 mobile__center">
         <img class="footer__logo mt-1 mb-3" src="<?php echo get_template_directory_uri() . '/assets/images/dawnlogo.png'; ?> "/>
            <ul class="footer__contact">
               <li class="mb-1">07980413955</li>
@@ -19,7 +19,7 @@
             </ul>
       </div>
       <div class="col-12 col-6 col-md-4">
-          <ul class="footer__blog__links">
+          <ul class="footer__blog__links__blogs">
               <li class="mb-1">Blog post</li>
               <li class="mb-1">Blog Post</li>
               <li class="mb-1">Blog Post</li>
@@ -35,23 +35,22 @@
               <li class="mb-1">Gallery</li>
               <li class="mb-1">Contact us</li>
             </ul>
-            <div class="d-flex justify-content-evenly">
+            <div class="d-flex justify-content-evenly lower__footer__logo">
                <img class="footer__sm__logo mb-3" src="<?php echo get_template_directory_uri() . '/assets/images/Pass-Plus-logo.jpeg'; ?> "/>
                <img class="footer__sm__logo mt-1 mb-3" src="<?php echo get_template_directory_uri() . '/assets/images/dvsa.jpeg'; ?> "/>
             </div>
       </div>
   </div>
-  <div>
+  <div class="lower__footer__location">
     <p class="text-center">Individual Driving Tuition. Refresher Driving Lessons. Intensive Driving Courses. Motorway Driving. Pass Plus.</p>
     <p class="text-center">Driving Lessons in Kent: West Malling, Maidstone, Sevenoaks, Borough Green.</p>
   </div>
-
-   <div class="companys__footer">
+</div>
+</footer>
+ <div class="companys__footer w-100">
     <p>Dawn driving tuition  © 2022 All rights Reserved </p>
     <p>Website by Ben West Web Design</p>
   </div>
-</div>
-</footer>
 <?php wp_footer(); ?>
 </body>
 
