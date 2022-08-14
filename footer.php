f80979485bd027cdd2cc027c95fc44c2eed8c36a
Ben West <benwest0291@outlook.com>
2022-08-14 14:21:36 +0100

    created footer on desktop

--- footer.php
+++ footer.php
@@ -6,6 +6,51 @@
       <input class="sign__up__input mt-1" type="text" placeholder="Enter your email">
     </div>
   </div>
+  <div class="main__footer">
+    <div class="container">
+    <div class="row">
+      <div class="col-12 col-6 col-md-4">
+        <img class="footer__logo mt-1 mb-3" src="<?php echo get_template_directory_uri() . '/assets/images/dawnlogo.png'; ?> "/>
+           <ul class="footer__contact">
+              <li class="mb-1">07980413955</li>
+              <li class="mb-1">dawn@drivingtuition.co.uk</li>
+              <div class="mt-2 mb-2"><i class="fa-brands fa-facebook-f"><i class="fa-brands fa-instagram"></i></i></div>
+              <li>Terms & Conditions</li>
+            </ul>
+      </div>
+      <div class="col-12 col-6 col-md-4">
+          <ul class="footer__blog__links">
+              <li class="mb-1">Blog post</li>
+              <li class="mb-1">Blog Post</li>
+              <li class="mb-1">Blog Post</li>
+              <li class="mb-1">Blog Post</li>
+              <li class="mb-1">Blog Post</li>
+              <li class="mb-1">Blog Post</li>
+              <li class="mb-1">Blog Post</li>
+            </ul>
+    </div>
+      <div class="col-12 col-6 col-md-4">
+          <ul class="footer__blog__links">
+              <li class="mb-1">About</li>
+              <li class="mb-1">Gallery</li>
+              <li class="mb-1">Contact us</li>
+            </ul>
+            <div class="d-flex justify-content-evenly">
+               <img class="footer__sm__logo mb-3" src="<?php echo get_template_directory_uri() . '/assets/images/Pass-Plus-logo.jpeg'; ?> "/>
+               <img class="footer__sm__logo mt-1 mb-3" src="<?php echo get_template_directory_uri() . '/assets/images/dvsa.jpeg'; ?> "/>
+            </div>
+      </div>
+  </div>
+  <div>
+    <p class="text-center">Individual Driving Tuition. Refresher Driving Lessons. Intensive Driving Courses. Motorway Driving. Pass Plus.</p>
+    <p class="text-center">Driving Lessons in Kent: West Malling, Maidstone, Sevenoaks, Borough Green.</p>
+  </div>
+
+   <div class="companys__footer">
+    <p>Dawn driving tuition  © 2022 All rights Reserved </p>
+    <p>Website by Ben West Web Design</p>
+  </div>
+</div>
 </footer>
 <?php wp_footer(); ?>
 </body>
--- inc/partials/masthead.php
+++ inc/partials/masthead.php
@@ -1,4 +1,4 @@
-<div class="banner" style="background-image: linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)), url(https://scontent-lhr8-2.xx.fbcdn.net/v/t39.30808-6/278567067_4765356113568614_4507541845570852674_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=BRYPXg3lwBEAX9imxKx&_nc_ht=scontent-lhr8-2.xx&oh=00_AT-Kuiwd1yeLzvjXu_GWJUprdIUCXTTOtEqfBRabiiP95Q&oe=62E1C3EC);">
+<div class="banner" style="background-image: linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)), url('https://scontent-lhr8-1.xx.fbcdn.net/v/t39.30808-6/287090855_4904447539659470_5547261970487734096_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=WgJsFSfQNXQAX9J1_D7&_nc_oc=AQlPTFK3RodU8kq_BXYiK6fnnoRWJU-qnNSxdPiWr7LXolJGKiZKOzznCmHujZh9cn0&_nc_ht=scontent-lhr8-1.xx&oh=00_AT8alekfw5ZdZrH3fjGwDFNyTtGHDTJ81-xYqH27tPCPrQ&oe=62FEB278');">
   <div class="container">
     <h1>Dawn Driving Tuition</h1>
     <p>Learn to drive with one of the best driving instructers in kent</p>
