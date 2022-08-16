1764156759d8f057ece52397b0101668ed282cf3
Ben West <benwest0291@outlook.com>
2022-08-16 11:30:35 +0100

    add template files

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<!-- include(get_template_directory() . '/inc/partials/masthead.php');
+
+
+include(get_template_directory() . '/inc/partials/blogcard.php');
+
+include(get_template_directory() . '/inc/partials/testimonial.php'); -->
+
+<?php get_footer(); ?>
---
+++ template-terms&con.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *  Template Name: Terms & Conditions
+ */
+get_header();
+?>
+
+
+
+<?php get footer(); ?>
